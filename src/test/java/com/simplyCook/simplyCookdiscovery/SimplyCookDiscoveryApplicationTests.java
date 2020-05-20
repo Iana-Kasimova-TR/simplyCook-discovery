ac3330e4feb9a9b8454ce97f12053ce24efd4f25
Iana.Kasimova <Smurfik55>
2020-05-20 16:30:17 +0300

    Initial commit

---
+++ src/test/java/com/simplyCook/simplyCookdiscovery/SimplyCookDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplyCook.simplyCookdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyCookDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
